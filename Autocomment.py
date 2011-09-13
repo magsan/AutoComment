40b659b0e7d6842d7788c0fcad65da43029f73c8
N Benatar <nbenatar@gmail.com>
2011-09-13 15:24:21 +0100

    Initial Version

---
+++ Autocomment.py
@@ -0,0 +1,18 @@
+def MakeSentence():
+
+    Sentence = ''
+    return Sentence 
+
+
+def createComment(Name, No_of_Lines):
+
+    for x in range(No_of_Lines):
+
+        Comment = MakeSentence()
+
+        
+
+def main():
+
+    Comment = createComment("Fred", "5")
+    print(Comment)
